5ad543a0b74a92838913b919775892d7d14849ae
ErichMoraga <33645296+ErichMoraga@users.noreply.github.com>
2019-01-26 16:05:01 -0600

    0.5.8 w/ speed-based Prius tuning tweaks

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -73,19 +73,20 @@ class CarInterface(object):
     tireStiffnessFront_civic = 192150
     tireStiffnessRear_civic = 202500
 
-    ret.steerKiBP, ret.steerKpBP = [[0.], [0.]]
+    ret.steerKiBP, ret.steerKpBP = [[0.,27.,47.], [0.,27.,47.]]
     ret.steerActuatorDelay = 0.12  # Default delay, Prius has larger delay
 
     if candidate == CAR.PRIUS:
+      stop_and_go = True
       ret.safetyParam = 66  # see conversion factor for STEER_TORQUE_EPS in dbc file
       ret.wheelbase = 2.70
       ret.steerRatio = 15.00   # unknown end-to-end spec
-      tire_stiffness_factor = 0.6371   # hand-tune
-      ret.mass = 3045 * CV.LB_TO_KG + std_cargo
-      ret.steerKpV, ret.steerKiV = [[0.4], [0.01]]
-      ret.steerKf = 0.00006   # full torque for 10 deg at 80mph means 0.00007818594
+      tire_stiffness_factor = 1.0
+      ret.mass = 3370 * CV.LB_TO_KG + std_cargo
+      ret.steerKpV, ret.steerKiV = [[0.369,0.397,0.418], [0.0056,0.0093,0.0121]]
+      ret.steerKf = 0.00007818594   # full torque for 10 deg at 80mph means 0.00007818594
       # TODO: Prius seem to have very laggy actuators. Understand if it is lag or hysteresis
-      ret.steerActuatorDelay = 0.25
+      ret.steerActuatorDelay = 0.16
 
     elif candidate in [CAR.RAV4, CAR.RAV4H]:
       ret.safetyParam = 73  # see conversion factor for STEER_TORQUE_EPS in dbc file
